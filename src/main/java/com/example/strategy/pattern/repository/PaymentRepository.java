9968fedd381690d53fa074af313fa2880e63c70b
tugce.caymaz <tuecyz06@outlook.com>
2026-08-05 11:42:48 +0300

    PaymentRepository oluşturuldu.

---
+++ src/main/java/com/example/strategy/pattern/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.example.strategy.pattern.repository;
+
+import com.example.strategy.pattern.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+    Optional<PaymentEntity> findByTransactionId(String transactionId);
+}
